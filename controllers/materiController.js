11f51303f3b1a8c81471de7b001f9ccd4857d62c
gilangaryo <gilang91668@gmail.com>
2024-01-18 10:34:55 +0700

    fix all

--- controllers/materiController.js
+++ controllers/materiController.js
@@ -27,109 +27,99 @@ const uploadssss = async (imgFile, portfolioFile, regis_id, res) => {
     }
 };
 
-
 const addMateri = async (req, res) => {
     try {
         const db = require('../config');
         const { uid } = req.params;
-        // , mentor_id, mentor_name
-        // const mentor_id = "xcpPYjTRcHBFM0gOMyZj";
-        // const mentor_name = "jeki";
         const { title, selectedCategory, selectedSubCategory, selectedSubMenu, price, learningPath } = req.body;
 
         console.log(uid);
         const registerDoc = await db.collection("mentor").doc(uid).get();
+
         if (!registerDoc.exists) {
             return res.status(404).json({ error: 'Mentor not found' });
         }
-        const name = registerDoc.data().nama;
-        console.log(name);
-        console.log("NAMANYAAAA : ", name);
 
-        // console.log(nama);
-        // console.log("halo", req.files);
-        // console.log("halo file", req.file);
+        const mentorName = registerDoc.data().nama;
 
-        // console.log(imgFile);
-        console.log("hai body", req.body);
+        console.log("Mentor Name:", mentorName);
+
+        const subCollectionRef3 = await createMateriDocument(db, uid, mentorName, title, selectedCategory, selectedSubCategory, selectedSubMenu, price, learningPath, req.files);
+
+        if (subCollectionRef3) {
+            res.status(201).send('Berhasil!');
+        } else {
+            res.status(500).send('Internal Server Error Red3');
+        }
+    } catch (error) {
+        console.error('Error in addMateri:', error);
+        res.status(500).send('Internal Server Error Error');
+    }
+};
+
+const createMateriDocument = async (db, uid, mentorName, title, selectedCategory, selectedSubCategory, selectedSubMenu, price, learningPath, files) => {
+    try {
         const categoryRef = db.collection('categories').doc(selectedCategory);
         const subCollectionRef = categoryRef.collection("subCategory").doc(selectedSubCategory);
         const subCollectionRef2 = subCollectionRef.collection("subMenu").doc(selectedSubMenu);
         const subCollectionRef3 = subCollectionRef2.collection("materi").doc();
 
+        await subCollectionRef3.set({
+            materi_id: subCollectionRef3.id,
+            mentor_id: uid,
+            mentor_name: mentorName,
+            title: title,
+            learning_path: learningPath,
+            price: price,
+            category: selectedCategory,
+            subCategory: selectedSubCategory,
+            subMenu: selectedSubMenu
+        });
 
+        const mentorCollection = db.collection('mentor').doc(uid);
+        const materiMentor = mentorCollection.collection("materi_mentor");
+        const materiMentorDocRef = materiMentor.doc(subCollectionRef3.id);
 
+        await materiMentorDocRef.set({
+            materi_id: subCollectionRef3.id,
+            mentor_id: uid,
+            title: title,
+            learningPath: learningPath,
+            price: price,
+            category: selectedCategory,
+            subCategory: selectedSubCategory,
+            subMenu: selectedSubMenu
+        });
 
-        if (uid) {
-            // const img = "https://firebasestorage.googleapis.com/v0/b/belajarin-ac6fd.appspot.com/o/imagess%2Fpythonnn.png?alt=media&token=b7910944-9334-4a39-aeab-20fe011f289a";
+        if (files && files.file[0] && files.file.length > 0) {
+            const imgFile = files.file[0];
+            const { appku } = require("../config");
+            const storageGet = getStorage(appku);
 
-            await subCollectionRef3.set({
-                materi_id: subCollectionRef3.id,
-                mentor_id: uid,
-                mentor_name: name,
-                title: title,
-                learning_path: learningPath,
-                price: price,
-                category: selectedCategory,
-                subCategory: selectedSubCategory,
-                subMenu: selectedSubMenu
+            const imgFilename = `materi/${uid}/${subCollectionRef3.id}/${imgFile.originalname}`;
+            const imgStorageRef = ref(storageGet, imgFilename);
+            await uploadBytes(imgStorageRef, imgFile.buffer, { contentType: imgFile.mimetype });
+            const imgDownloadURL = await getDownloadURL(imgStorageRef);
+
+            await subCollectionRef3.update({
+                image: imgDownloadURL
             });
 
-            const mentorCollection = db.collection('mentor').doc(uid);
-            const materiMentor = mentorCollection.collection("materi_mentor");
-            const materiMentorDocRef = materiMentor.doc(subCollectionRef3.id);
-            await materiMentorDocRef.set({
-                materi_id: subCollectionRef3.id,
-                mentor_id: uid,
-                title: title,
-                learningPath: learningPath,
-                price: price,
-                category: selectedCategory,
-                subCategory: selectedSubCategory,
-                subMenu: selectedSubMenu
+            await materiMentorDocRef.update({
+                image: imgDownloadURL
             });
-            console.log("hai berhasil masuk mentor : ", req.files.file[0]);
-            // console.log("hai berhasil masuk mentor : ", req.files.file[0]);
 
-            // Check if req.files.image is defined and has at least one element
-            if (req.files && req.files.file[0] && req.files.file.length > 0) {
-                const imgFile = req.files.file[0];
-                const { appku } = require("../config");
-                const storageGet = getStorage(appku);
-
-                // Handle the img file
-                const imgFilename = `materi/${uid}/${subCollectionRef3.id}/${imgFile.originalname}`;
-                const imgStorageRef = ref(storageGet, imgFilename);
-                await uploadBytes(imgStorageRef, imgFile.buffer, { contentType: imgFile.mimetype });
-                const imgDownloadURL = await getDownloadURL(imgStorageRef);
-
-                await subCollectionRef3.update({
-                    image: imgDownloadURL
-                });
-
-                await materiMentorDocRef.update({
-                    image: imgDownloadURL
-                });
-
-                console.log('Files successfully uploaded. img Download URL:', imgDownloadURL);
-
-            } else {
-                // Handle the case when req.files.image is undefined or empty
-                console.error('Image file is missing or empty.');
-            }
-
-
-            res.status(201).send('Berhasil!');
+            console.log('Files successfully uploaded. img Download URL:', imgDownloadURL);
         } else {
-            res.status(400).send('uid tidak ada');
+            console.error('Image file is missing or empty.');
         }
+
+        return subCollectionRef3;
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+        console.error('Error creating materi document:', error);
+        return null;
     }
-
 };
-
 
 // const getMateriMentorsatu = async (req, res) => {
 //     const { uid } = req.params;
